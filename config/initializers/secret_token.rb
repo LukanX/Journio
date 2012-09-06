ec53f53b174a30384f2d5fb90f03b600d7ea3eb2
Lance Cookson <lansing.cookson@gmail.com>
2012-09-06 08:25:38 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Journio::Application.config.secret_token = 'fbce4afbca715df9a7da6a52f93f6b5b8aa1fbe82e2efed007cf44c8de581f33709677495c53a4f60194d47d32c1ecfabd9486a57e85d147bad489fbfb215669'
