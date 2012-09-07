377483ea0eb48ec30ae252f38f7a8a30e92c74b7
Lance Cookson <lansing.cookson@gmail.com>
2012-09-07 15:55:02 -0400

    Even more twilio attempts

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,9 @@
 class MessagesController < ApplicationController
 	def create
-		content:(params["Body"])
-	    from_number:(params["From"])
-	    to_number:(params["To"])
-	    @message = current_study.messages.new(params[:message])
+		content = params["Body"]
+	    from_number = params["From"]
+	    to_number = params["To"]
+	    @message = current_study.messages.new(:content = content, :from_number = from_number, :to_number = to_number)
 	    @message.save
 	end
 end
