f4a6d2fbb836a1d94aeb87d3b902c0c4f7b3fd58
Lance Cookson <lansing.cookson@gmail.com>
2012-09-07 15:36:27 -0400

    Attempt to receive messages from Twilio

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,7 @@
 class Message < ActiveRecord::Base
   attr_accessible :content, :from_number, :study_id, :to_number
+  belongs_to :study
+
+  validates :content, presence: true, length: { maximum: 160 }
+
 end
