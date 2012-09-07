899649533d66092d208474e888f31f1ffc7a22cb
Lance Cookson <lansing.cookson@gmail.com>
2012-09-07 12:01:25 -0400

    Took out content of messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,4 @@
 class MessagesController < ApplicationController
 	def create
-		:content = params["Body"]
-	    :from_number = params["From"]
-	    :to_number = params["To"]
-	    @message = current_study.messages.new(params[:message])
-	    @message.save
 	end
 end
