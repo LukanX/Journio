e2305f6d622681fbef1b3ffa07c2dbc1fb55813f
Lance Cookson <lansing.cookson@gmail.com>
2012-09-07 15:42:50 -0400

    Another try at Twilio integration

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,8 @@
 class MessagesController < ApplicationController
 	def create
-		:content = params["Body"]
-	    :from_number = params["From"]
-	    :to_number = params["To"]
+		content = params["Body"]
+	    from_number = params["From"]
+	    to_number = params["To"]
 	    @message = current_study.messages.new(params[:message])
 	    @message.save
 	end
