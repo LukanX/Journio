b4e0275f718ae673835e15c4df405ae86961f410
Lance Cookson <lansing.cookson@gmail.com>
2012-09-07 15:57:54 -0400

    Twilio

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
 		content = params["Body"]
 	    from_number = params["From"]
 	    to_number = params["To"]
-	    @message = current_study.messages.new(:content = content, :from_number = from_number, :to_number = to_number)
+	    @message = current_study.messages.new(params[:message])
 	    @message.save
 	end
 end
