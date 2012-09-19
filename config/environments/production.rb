df96e366de2e33027f744a0ae59ff1ddeb8681ec
Lance Cookson <lansing.cookson@gmail.com>
2012-09-19 09:47:06 -0400

    Removed authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 Journio::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  config.middleware.insert_after(::Rack::Lock, "::Rack::Auth::Basic", "Staging") do |u, p|
-    [u, p] == ['lsux', 'UXDes1gn!']
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
